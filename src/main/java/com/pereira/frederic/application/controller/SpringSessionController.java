aa3aafde6ab251e983238c00498aadef1c7077b5
frpereira <frederic-michel.pereira@softeam.fr>
2019-07-16 15:22:48 +0200

    innt

---
+++ src/main/java/com/pereira/frederic/application/controller/SpringSessionController.java
@@ -0,0 +1,65 @@
+package com.pereira.frederic.application.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSessionController {
+	
+	
+	@GetMapping("/read-message")
+	public List<String> process(Model model, HttpSession session) {
+		@SuppressWarnings("unchecked")
+		List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+		if (messages == null) {
+			messages = new ArrayList<>();
+		}
+		model.addAttribute("sessionMessages", messages);
+		return (messages);
+	}
+
+	@PostMapping("/persist-message")
+	public ResponseEntity<String> persistMessage(@RequestBody String msg, HttpServletRequest request) {
+		@SuppressWarnings("unchecked")
+		List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
+		if (messages == null) {
+			messages = new ArrayList<>();
+			request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+		}
+		messages.add(msg);
+		request.getSession().setAttribute("MY_SESSION_MESSAGES", messages);
+		return ResponseEntity.status(HttpStatus.OK)
+		        .body("message in session");
+	}
+
+	@PostMapping("/destroy")
+	public ResponseEntity<String> destroySession(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return ResponseEntity.status(HttpStatus.OK)
+		        .body("destroy messages");
+	}
+	
+	@RequestMapping(value="/ping", method=RequestMethod.GET)
+	public ResponseEntity<String> ping() {
+		return ResponseEntity.status(HttpStatus.OK)
+		        .body("pong");
+	}
+}
---
+++ src/main/resources/db/migration/V1.0__InitBase.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS "spring_session"."user";
+
+DROP SEQUENCE  IF EXISTS spring_session.ID_SEQ;
+
+CREATE SEQUENCE spring_session.ID_SEQ;
