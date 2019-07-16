9838731c8b7ffe72bfc37bffe985c90ed6d91bcb
frpereira <frederic-michel.pereira@softeam.fr>
2019-07-16 11:34:45 +0200

    messages en session

--- src/main/java/com/pereira/frederic/application/ssession/SpringSessionApplication.java
+++ src/main/java/com/pereira/frederic/application/ssession/SpringSessionApplication.java
@@ -2,11 +2,11 @@ package com.pereira.frederic.application.ssession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringSessionApplication {
 
 	public static void main(String[] args) {
+	    System.setProperty("server.servlet.context-path", "/session");
 		SpringApplication.run(SpringSessionApplication.class, args);
 	}
 
--- src/main/resources/db/migration/V1.0__InitBase.sql
+++ src/main/resources/db/migration/V1.0__InitBase.sql
@@ -1,9 +1,5 @@
 DROP TABLE IF EXISTS "spring_session"."user";
 
-
-DROP SCHEMA  IF EXISTS spring_session CASCADE;
 DROP SEQUENCE  IF EXISTS spring_session.ID_SEQ;
 
-
-CREATE SCHEMA spring_session;
 CREATE SEQUENCE spring_session.ID_SEQ;
